e636734cb5ca5a3ad81d287140b1e2d468188bb7
Lribeiro <code.resources2024@gmail.com>
2026-02-28 16:41:57 +0100

    feat: refactor main.tsx to use ClientInformation with Suspense and remove InstagromApp; update Seguimiento.md with new course link

--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -1,12 +1,20 @@
-import { StrictMode } from 'react'
+import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Toaster } from 'sonner'
 import './index.css'
-import { InstagromApp } from './useOptimistic/InstagromApp'
+import { ClientInformation } from './08-use-suspense/ClientInformation'
+import { getUserAction } from './08-use-suspense/api/get-user.actions'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Toaster />
-    <InstagromApp />
+    <Suspense fallback={
+      <div className='bg-gradient flex flex-col gap-4'>
+        <h2 className="text-4xl font-thin text-white"> CARGANDO</h2>
+      </div>
+    }>
+
+      <ClientInformation getUser={getUserAction(150)} />
+    </Suspense>
   </StrictMode>,
 )
--- Seguimiento.md
+++ Seguimiento.md
@@ -1,3 +1,3 @@
 ### Voy por aqui -->
 
-https://cursos.devtalles.com/courses/take/react-de-cero/lessons/66424299-use-api-suspense-preparacion-de-ejercicio
+https://curscdos.devtalles.com/courses/take/react-de-cero/lessons/66481513-continuacion-de-proyecto-hooksapp
